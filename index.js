7ab6e490652751162735d089eb2bcb13feeed531
Mike Chevett <mchevett@gmail.com>
2023-09-05 09:00:07 -0400

    use path.join

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { dirname } from 'path';
+import { dirname, join } from 'path';
 import { glob } from 'glob';
 import { fileURLToPath } from 'url';
 
@@ -6,19 +6,22 @@ import { fileURLToPath } from 'url';
 export default async function (meta, ...globPaths) {
 	const __dirname = dirname(fileURLToPath(meta.url));
 
-	const paths = await Promise.resolve(globPaths)
+	const paths = await Promise.resolve(globPaths.flat())
 		.map(path => {
-			const fullPath = `${__dirname}${path}`;
+			const fullPath = join(__dirname, path);
+			console.log(`globbing "${fullPath}"`);
 			return glob(fullPath);
 		})
 		.then(lst => lst.flat());
 
-
 	while (paths.length) {
 		const path = paths.shift();
-		console.log(`------------------------------`);
-		console.log(`loading "${path}"`);
-		console.log(`==============================`);
-		await import(path);
+
+		await import(path)
+			.catch(err => {
+				console.error(`failed to load "${path}"`);
+				console.error(err);
+				throw err;
+			});
 	}
 }
